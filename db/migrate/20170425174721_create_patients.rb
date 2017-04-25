9b3b7f519376dccc46712f9568105bc30d841e27
Fatima <fatimaalrubayee@gmail.com>
2017-04-25 17:51:02 +0000

    rails g scaffold

---
+++ db/migrate/20170425174721_create_patients.rb
@@ -0,0 +1,14 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.boolean :state
+      t.references :doctor, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
