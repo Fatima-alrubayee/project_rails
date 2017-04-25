929bbcc126ee544fd89a633bdf7b15313c9463a7
Fatima <fatimaalrubayee@gmail.com>
2017-04-25 20:52:35 +0000

    stayle

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,7 +3,7 @@ class Doctor < ActiveRecord::Base
     
    validates_presence_of :license_no
    validates_uniqueness_of :license_no,  case_sensitive: false
-   
+   validates_format_of :phone_number, with:  /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
     def doctor_info
         "#{first_name} #{last_name} #{email} #{phone_number}"
     end
