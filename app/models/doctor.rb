82c83be25178e3ec49f9bb74203a39b2a52f3e5f
Fatima <fatimaalrubayee@gmail.com>
2017-04-25 19:10:42 +0000

    uniquely identified

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,6 @@
 class Doctor < ActiveRecord::Base
     has_many :patients
+    
+   validates_presence_of :license_no
+   validates_uniqueness_of :license_no,  case_sensitive: false
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,7 @@
 class Patient < ActiveRecord::Base
   belongs_to :doctor
+  
+   validates_presence_of :first_name, :last_name
+   validates_uniqueness_of :first_name, :last_name, :doctor_id,  case_sensitive: false
+ 
 end
