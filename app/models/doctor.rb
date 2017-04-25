0d1667a480b8dc6d49f60a0ce3cfd474886aeb16
Fatima <fatimaalrubayee@gmail.com>
2017-04-25 19:32:55 +0000

    display information

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,8 @@ class Doctor < ActiveRecord::Base
     
    validates_presence_of :license_no
    validates_uniqueness_of :license_no,  case_sensitive: false
+   
+    def doctor_info
+        "#{first_name} #{last_name} #{email} #{phone_number}"
+    end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,5 +3,10 @@ class Patient < ActiveRecord::Base
   
    validates_presence_of :first_name, :last_name
    validates_uniqueness_of :first_name, :last_name, :doctor_id,  case_sensitive: false
+   
+   
+   def full_patients
+       "#{first_name} #{last_name} #{phone_number} #{email}"
+    end
  
 end
